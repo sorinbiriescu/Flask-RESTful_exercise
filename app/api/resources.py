dc257f1ccb5c7b67d127224f812549730dcc0a91
Sorin <sorin.biriescu@gmail.com>
2018-09-22 15:32:28 +0200

    managed to make work the fields.Url thingy

--- app/api/resources.py
+++ app/api/resources.py
@@ -1,7 +1,8 @@
 from flask import Blueprint
-from flask_restful import Api, Resource, url_for, reqparse, fields,marshal_with, abort
+from flask_restful import Api, Resource, url_for, reqparse, fields, marshal_with, abort
 from app.models import Message
 from app.main import db
+from app.api.status import *
 
 api_bp = Blueprint('api', __name__)
 api = Api(api_bp)
@@ -9,13 +10,14 @@ api = Api(api_bp)
 resource_fields = {
     "id": fields.Integer,
     "message": fields.String,
-    "message_author": fields.String
+    "message_author": fields.String,
+    "URL": fields.Url("api.message", absolute=True)
 }
 
 def get_message_or_404(id):
     message = Message.query.filter_by(id=id).first()
     if message is None:
-        abort(404, message="Message {} doesn't exist".format(id))
+        abort(HTTP_404_NOT_FOUND, message="Message {} doesn't exist".format(id))
     else:
         return message
 
@@ -23,7 +25,7 @@ class MessageList(Resource):
     @marshal_with(resource_fields)
     def get(self):
         message_list = Message.query.all()
-        return [message for message in message_list]
+        return [message for message in message_list], HTTP_200_OK
 
     @marshal_with(resource_fields)
     def post(self):
@@ -38,13 +40,13 @@ class MessageList(Resource):
 
         db.session.add(new_message)
         db.session.commit()
-        return new_message, 200
+        return new_message, HTTP_201_CREATED
 
 class MessageItem(Resource):
     @marshal_with(resource_fields)
     def get(self, id):
         message = get_message_or_404(id)
-        return message
+        return message, HTTP_200_OK
 
 
     @marshal_with(resource_fields)
@@ -60,14 +62,13 @@ class MessageItem(Resource):
             message.message_author = args["message_author"]
         db.session.commit()
 
-        return message
+        return message, HTTP_202_ACCEPTED
 
 
     def delete(self,id):
         message = get_message_or_404(id)
         db.session.delete(message)
         db.session.commit()
-        return {"Message":"Message deleted"}, 200
+        return {"Message":"Message deleted"}, HTTP_200_OK
 
-api.add_resource(MessageList, '/message')
-api.add_resource(MessageItem, '/message/<int:id>')
+
--- app/main.py
+++ app/main.py
@@ -10,10 +10,9 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 from logzero import logger
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_restful import Api
 
 db = SQLAlchemy()
-
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
@@ -29,9 +28,13 @@ def create_app():
 
     from app.views import main_routes
     from app.api.resources import api_bp
-    
+
     app.register_blueprint(main_routes)
     app.register_blueprint(api_bp)
+
+    from app.api.resources import api,MessageItem, MessageList
+    api.add_resource(MessageList, '/message','/message/', endpoint="message_list")
+    api.add_resource(MessageItem, '/message/<int:id>', endpoint="message")
 
     return app
 
