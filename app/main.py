0ebd84a7da64151d2f4efd890520d24f1c0e70b3
Sorin <sorin.biriescu@gmail.com>
2018-09-22 12:58:09 +0200

    Finally managed to create the connection string ...

--- app/main.py
+++ app/main.py
@@ -9,11 +9,15 @@ import os
 from flask import Flask, jsonify
 from flask_cors import CORS
 from logzero import logger
-from app.views import main_routes
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 
-def create_app(config=None):
+def create_app():
     app = Flask(__name__, instance_relative_config=True)
+    app.config.from_pyfile('development.py')
+    db.init_app(app)
 
     # Setup cors headers to allow all domains
     # https://flask-cors.readthedocs.io/en/latest/
@@ -22,6 +26,7 @@ def create_app(config=None):
     # Definition of the routes. Put them into their own file. See also
     # Flask Blueprints: http://flask.pocoo.org/docs/latest/blueprints
 
+    from app.views import main_routes
     app.register_blueprint(main_routes)
     return app
 
